bb2367fc88ecb1a19157b959ac189d536dd8e0d0
Talionidas <thierry.a.stahl@gmail.com>
2024-06-14 16:22:41 +0200

    Assignment Week 1

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// select the DOM elements for output
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const lastModified = document.lastModified;
+    document.getElementById("date-time").textContent = lastModified;
+});
